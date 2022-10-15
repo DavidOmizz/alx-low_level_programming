16dee18f35b786f8f45cddfafc39520ca5065eda
David <davidomisakin4good@gmail.com>
2022-10-15 08:29:04 -0700

    Done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 49; a <58; a++)
+	{
+		for (b = 49; b < 58; m++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != 55 || b != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
