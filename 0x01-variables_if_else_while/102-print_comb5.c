123051aa46fc802de235cee94ba6136bd48f636b
David <davidomisakin4good@gmail.com>
2022-10-15 08:47:27 -0700

    Done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdi0.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || a !=99)
+				{
+					putchar(',');
+					puchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
