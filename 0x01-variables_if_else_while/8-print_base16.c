847bac869ff4534dde0c54086e8b697e0f188314
David <davidomisakin4good@gmail.com>
2022-10-15 08:58:46 -0700

    Done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char m;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (m = 'a'; m < 'f'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
