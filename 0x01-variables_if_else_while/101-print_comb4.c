d7a36e01d13654305ed40c4919ee7916fa86b5bb
David <davidomisakin4good@gmail.com>
2022-10-15 08:35:45 -0700

    Done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,19 +8,22 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 49; a <58; a++)
+	for (a = 49; a < 58; a++)
 	{
 		for (b = 49; b < 58; b++)
 		{
 			for (c = 50; c < 58; c++)
 			{
-				putchar(a);
-				putchar(b);
-				putchar(c);
-				if (a != 55 || b != 56)
+				if (c > b && b > a)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
