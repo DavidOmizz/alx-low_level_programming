31d743fe9368cea770137f2c4ba9673f6b3f3f5e
David <davidomisakin4good@gmail.com>
2022-10-22 05:59:58 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int N;
+	char ch;
+
+	for (N = 0; N <= 9; N++)
+	{
+		for (ch = 'a'; ch <= 'z' ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
